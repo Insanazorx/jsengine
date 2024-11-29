0e847a438c299327953eb8f36ea71d609b6b3f52
Insanazorx <127684390+Insanazorx@users.noreply.github.com>
2024-11-29 15:02:03 +0300

    Delete debug.h

--- debug.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#define GREEN_TEXT "\033[1;32m"
-#define RESET_TEXT "\033[0m"
-#define DEBUG(x) do {std::cout <<"[" GREEN_TEXT "+" RESET_TEXT "] " << x << std::endl;char _; std::cin >> std::noskipws >> _ ;} while (0)
-#define TODO() DEBUG("TODO!")
-
-
-
